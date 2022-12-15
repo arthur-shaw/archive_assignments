1e7e91d70d0b72fb0bf437dc07980394cee32e8d
Arthur Shaw <47256431+arthur-shaw@users.noreply.github.com>
2022-12-14 22:20:51 -0500

    If versions specified, get those; if not specified, get all verions

--- R/get_matching_assignments.R
+++ R/get_matching_assignments.R
@@ -1,11 +1,11 @@
 #' Get matching assignments
-#' 
+#'
 #' Specify questionnaire ID and, optionally, one or more version
-#' 
+#'
 #' @param qnr_id Character. Questionnaire ID as GUID
 #' @param qnr_versions Numeric vector. Questionnaire versions(s)
 #' @inheritParams susoflows::download_matching
-#' 
+#'
 #' @importFrom dplyr case_when
 #' @importFrom purrr map_dfr
 #' @importFrom susoapi get_assignments
@@ -15,15 +15,20 @@ get_matching_assignments <- function(
     server = Sys.getenv("SUSO_SERVER"),
     workspace = Sys.getenv("SUSO_WORKSPACE"),
     user = Sys.getenv("SUSO_USER"),
-    password = Sys.getenv("SUSO_PASSWORD")    
+    password = Sys.getenv("SUSO_PASSWORD")
 ) {
 
+    if (is.null(qnr_versions)) {
+        qnr_versions_server <- dplyr::pull(susoapi::get_questionnaires(), version)
+    }
+
     # convert `qnr_versions` into format expected by `susoapi::get_assignments()`
-    # NULL -> ""
+    # NULL -> get dataframe of questionnaires and extract the versions
     # numeric vector -> character vector
-    qnr_versions <- dplyr::case_when(
-        is.null(qnr_versions) ~ "",
-        is.numeric(qnr_versions) ~ as.character(qnr_versions)
+    qnr_versions <- ifelse(
+        test = is.null(qnr_versions),
+        yes = as.character(qnr_versions_server),
+        no =  as.character(qnr_versions)
     )
 
     # collect assignments for given questionnaire ID and, optionally, version(s)
@@ -34,7 +39,7 @@ get_matching_assignments <- function(
             qnr_version = .x
         )
     )
-    
+
     return(assignments)
 
 }
